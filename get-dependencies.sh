75cefed981146d3f820b4964e584572fda5e6615
Link4Electronics <link4electronics@gmail.com>
2026-01-27 23:45:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm libweaver
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
