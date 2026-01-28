7bb86f096d96a5efdcae9d964225e19874f95ba9
Link4Electronics <link4electronics@gmail.com>
2026-01-28 01:37:06 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,13 +8,13 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/scalable/apps/org.legoisland.Isle.svg
-export DESKTOP=/usr/share/applications/org.legoisland.Isle.desktop
+export ICON=/$HOME/.local/share/icons/hicolor/scalable/apps/org.legoisland.Isle.svg
+export DESKTOP=/$HOME/.local/share/applications/org.legoisland.Isle.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/isle /usr/bin/isle-config
+quick-sharun /$HOME/.local/bin/isle /usr/bin/isle-config
 #/usr/lib/liblego1.so
 
 # Additional changes can be done in between here
