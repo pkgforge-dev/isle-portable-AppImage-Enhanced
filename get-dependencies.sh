7517587ea16d5c7403aebf5057cae389980432a0
Link4Electronics <link4electronics@gmail.com>
2026-02-03 01:26:56 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,7 +21,7 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package
 
 # If the application needs to be manually built that has to be done down here
-echo "Making nightly build of  RigelEngine..."
+echo "Making nightly build of isle-portable..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/isledecomp/isle-portable"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
