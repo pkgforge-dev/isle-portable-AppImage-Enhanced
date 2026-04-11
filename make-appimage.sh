cbdf19192d1c66ce288fa8fd6ca440d86dfcbbe2
Link4Electronics <link4electronics@gmail.com>
2026-04-11 20:19:31 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.legoisland.Isle.svg
 export DESKTOP=/usr/share/applications/org.legoisland.Isle.desktop
@@ -17,7 +17,9 @@ quick-sharun /usr/bin/isle-config /usr/bin/isle
 mv /usr/lib/liblego1.so ./AppDir/bin
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
