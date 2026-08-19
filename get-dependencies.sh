f0d4ef3186e2b3e08718d9fc3cef812a1f5f3487
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:07:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,33 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    cmake     \
-    iniparser \
-    libdecor  \
-    python    \
-    sdl3
+pacman -Syu --noconfirm cmake iniparser sdl3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
-echo "Making nightly build of isle-portable..."
+echo "Building isle-portable..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/isledecomp/isle-portable"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
 git clone --recursive --depth 1 "$REPO" ./isle-portable
 echo "$VERSION" > ~/version
 
-cd ./isle-portable
-mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-         -DCMAKE_INSTALL_PREFIX=/usr \
-         -DDOWNLOAD_DEPENDENCIES=ON
-make -j$(nproc)
-make install
+cmake -S ./isle-portable -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DDOWNLOAD_DEPENDENCIES=ON
+cmake --build build -j$(nproc)
+cmake --install build
 sed -i 's/^Exec=isle/Exec=isle-config/' "/usr/share/applications/org.legoisland.Isle.desktop"
